a89ab3129a379953678ed5d65eed019afffda0cf
Julio <jucesargois@gmail.com>
2022-07-15 15:17:06 -0300

    Created model: Person

--- myApp.js
+++ myApp.js
@@ -3,10 +3,26 @@ let express = require('express');
 
 
 
-const mongoose = require('mongoose');
+let mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+
+
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+
+});
+
+
+const Person = mongoose.model('Person', personSchema)
+
+
+
+
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
